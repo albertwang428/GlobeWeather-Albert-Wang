f1eb51eecf9366f07d059603cbaf7e54358c0efb
albertwang428 <davidwangnju@163.com>
2017-03-21 13:48:36 +0800

    Update sample.js

--- sample.js
+++ sample.js
@@ -2,6 +2,7 @@ var sp;
 var rdr;
 var scene;
 var camera;
+var texture;
 function sample(){
 	scene=new THREE.Scene();
 	camera=new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,1000);
@@ -15,20 +16,19 @@ function sample(){
 	light.position.set(0,60,30);
 	scene.add(light);
 	//创建图片为材质
-	var texture1=THREE.ImageUtils.loadTexture("image/continental.png");
-	texture1.magFilter=THREE.LinearFilter;
-	texture1.minFilter=THREE.LinearFilter;
+	texture=THREE.ImageUtils.loadTexture('image/coast.png');
+	texture.magFilter=THREE.LinearFilter;
+	texture.minFilter=THREE.LinearFilter;
+
 	var texture2=THREE.ImageUtils.loadTexture("image/coast.png");
 	texture2.magFilter=THREE.LinearFilter;
 	texture2.minFilter=THREE.LinearFilter;
-	var gm=new THREE.SphereGeometry(150,40,40,0,Math.PI*2,0,Math.PI);
+	var gm=new THREE.SphereGeometry(10,40,40,0,Math.PI*2,0,Math.PI);
 	
 	var uniforms={
-		Continental:{type:'t',value:0},
+		Continental:{type:'t',value:1,texture:texture},
 		Coast:{type:'t',value:0}
 	}
-
-	uniforms["Continental"].texture=texture1;
 
 	var vertShader=document.getElementById("globeVertexShader").innerHTML;
 	var fragShader=document.getElementById("globeFragmentShader").innerHTML;
@@ -40,12 +40,15 @@ function sample(){
 	});
 
 	sp=new THREE.Mesh(gm,mt);
+	sp.position.x=10;
+	sp.position.y=10;
+	sp.position.z=10;
 
 	scene.add(sp);
 
 	camera.position.x=30;
 	camera.position.y=30;
-	camera.position.z=30;
+	camera.position.z=100;
 	camera.lookAt(new THREE.Vector3(0,0,0));
 
 	document.getElementById("container").appendChild(rdr.domElement);
@@ -56,8 +59,8 @@ function renderScene(){
 	//step+=0.04;
 	//sp.position.x=20+(10*(Math.cos(step)));
 	//sp.position.y=2+(10*Math.abs(Math.sin(step)));
-
-	//requestAnimationFrame(renderScene);
+	//texture.needsUpdate=true;
+	requestAnimationFrame(renderScene);
 	rdr.render(scene,camera);
 }
 window.onload=sample;
